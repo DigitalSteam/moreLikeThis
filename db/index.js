a84b0f118aeefd08d898b34d93cf9a825ef233b0
Nils Gudbranson <nils.gudbran@gmail.com>
2018-07-23 16:11:16 -0700

    Finished the work on populating the database using faker and making sure that all of the information is formatted in a way that is as accurate as possible. In other words, not just throwing only randomly generated words in for everything; attempted to make the data semi-realistic

--- db/index.js
+++ db/index.js
@@ -3,15 +3,40 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  database: 'relevant'
+  password: 'root',
+  database: 'relevant',
 });
 
-connection.connect((err) => {
-  if (err) {
-    throw err;
-  } else {
-    console.log('Connected!');
-  }
-});
+const popDb = (infoObj, cb) => {
+  const randomInt = Math.floor(Math.random() * 11);
+  connection.query(`INSERT INTO games (name, description, release_date, price, average_review, thumbnail) VALUES ('${infoObj.name}', '${infoObj.description}', '${infoObj.release_date}', ${infoObj.price}, '${randomInt}/10', '${infoObj.thumbnail}')`, (err, data) => {
+    if (err) {
+      cb(err, null);
+    } else {
+      cb(null, data);
+    }
+  });
+};
 
-exports.connection = connection;
+const popGenres = (genreName) => {
+  connection.query(`INSERT INTO genres (name) VALUES ('${genreName}')`, (err) => {
+    if (err) {
+      throw err;
+    }
+  });
+};
+
+const popLink = (currentGame) => {
+  const randomNum = Math.ceil(Math.random() * 11);
+  connection.query(`INSERT INTO link (genreId, gameId) VALUES ((SELECT id FROM genres WHERE genres.id=${randomNum}), (SELECT id FROM games WHERE games.name='${currentGame}'))`, (err) => {
+    if (err) {
+      throw err;
+    }
+  });
+};
+
+module.exports = {
+  popDb,
+  popGenres,
+  popLink,
+};
